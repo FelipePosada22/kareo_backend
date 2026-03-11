36a7a105b6da51712069f1cde0070af684a07044
Felipe Posada <fromero@sms-sudamerica.com>
2026-03-10 22:43:40 -0500

    feat(schedules): register schedule routes

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,19 @@ import patientRoutes from "./modules/patients/patient.routes";
 import appointmentRoutes from "./modules/appointments/appointment.routes";
 import professionalRoutes from "./modules/professionals/professional.routes";
 import appointmentTypeRoutes from "./modules/appointment-types/appointmentType.routes";
+import scheduleRoutes from "./modules/schedules/schedule.routes";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 app.use("/auth", authRoutes);
 app.use("/patients", patientRoutes);
 app.use("/appointments", appointmentRoutes);
 app.use("/professionals", professionalRoutes);
 app.use("/appointment-types", appointmentTypeRoutes);
+app.use("/schedules", scheduleRoutes);
 
 
 app.get("/me", authMiddleware, (req: any, res) => {
