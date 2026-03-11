424e031818d69356e9894233c2ba3a3ba1ab8903
Felipe Posada <fromero@sms-sudamerica.com>
2026-03-10 22:01:33 -0500

    feat(auth): add protected test route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes";
+import { authMiddleware } from "./shared/middleware/auth.middleware";
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 
 console.log(process.env.DATABASE_URL);
 app.use("/auth", authRoutes);
+
+app.get("/me", authMiddleware, (req: any, res) => {
+  res.json(req.user);
+});
 
 app.get("/", (req, res) => {
   res.send("Medora API running");
